cdfc6b648274506703e4aa9006b9ee133edf09a2
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2024-01-08 23:55:18 -0300

    corrigindo problema da limitação da API,
    Co-authored-by: DevWyl  <wylkerson03@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
+import asyncio
 import keyring
 import hikari
 from hikari import intents
-import asyncio
+from engines.techonology import technology
 
-from engines.business2 import get_news
-
-channel_business = keyring.get_password('bot_news', 'channel_business')
+channel_business = keyring.get_password('bot_news', 'channel_news_brasil')
 sent_news = []
 
 bot = hikari.GatewayBot(keyring.get_password('bot_news', 'token'), intents=intents.Intents.ALL)
 
 @bot.listen()
 async def on_started(event: hikari.StartedEvent) -> None:
-  
-    # Google News Business
-    results = await get_news()
-    for result in results:
-        if result[0] not in sent_news:
-            sent_news.append(result[0])
-            news_info = f'{"-"*50}\nTÍTULO DA NOTICIA: {result[0]}\nDATA: {result[1]}\nLINK: {result[2]}\nEMPRESA: {result[3]}'
-            await bot.rest.create_message(channel_business, news_info)
-            await asyncio.sleep(10)
-    await asyncio.sleep(60)
+    while True:
+        results = await technology()
+        for result in results:
+            if result[0] not in sent_news:
+                sent_news.append(result[0])
+                news_info = f'{"-"*50}\nTÍTULO DA NOTICIA: {result[0]}\nDATA: {result[1]}\nLINK: {result[2]}\nEMPRESA: {result[3]}'
+                await bot.rest.create_message(channel_business, news_info)
+                await asyncio.sleep(30)
+        await asyncio.sleep(86400)  # Sleep for one day
 
 bot.run()
